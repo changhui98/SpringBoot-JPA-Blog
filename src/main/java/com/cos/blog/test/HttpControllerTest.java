751655466374acc3417fb1956440700f2817b100
unknown <net_0102@naver.com>
2024-07-28 19:24:51 +0900

    yml 파일 설정 완료

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -14,11 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpControllerTest {
 
+	private static final String TAG = "HttpControllerTest : ";
+	
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		Member m = Member.builder().username("ssar").password("1234").email("ssar@nate.com").build();
+		System.out.println(TAG+"getter : " + m.getUsername());
+		m.setUsername("cos");
+		System.out.println(TAG+"getter : " + m.getUsername());
+		return "lombok test 완료";
+
+	}
+	
 	// 인터넷 브라우저 요청은 무조건 get요청밖에 할 수 없다.
 	// http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
 	public String getTest(Member m) {
-		
+
 		return "get 요청 : " + m.getId() + ", " + m.getUsername()+ " ," + m.getPassword()+ " ," + m.getEmail();
 	}
 	
@@ -32,6 +44,7 @@ public class HttpControllerTest {
 	// http://localhost:8080/http/put
 	@PutMapping("/http/put")
 	public String putTest(@RequestBody Member m) {
+		
 		return "put 요청 :" + m.getId() + ", " + m.getUsername()+ " ," + m.getPassword()+ " ," + m.getEmail();
 	}
 
