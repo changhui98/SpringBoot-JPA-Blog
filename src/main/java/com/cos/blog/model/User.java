79fbd795245d8e90821a9dc99b80cd7e28f9c2b7
unknown <net_0102@naver.com>
2024-07-31 19:14:53 +0900

    create board table

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -11,6 +11,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+// ORM -> Java(다른언어) Object -> 테이블로 매핑해주는 기술 
 @Entity // User 클래스가 MySQL 에 테이블이 생성이 된다. 
 public class User {
 	
