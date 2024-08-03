45d5bf9f9191022a3e171c976ed909f23d286f78
unknown <net_0102@naver.com>
2024-08-03 18:19:28 +0900

    페이징 기능 수정

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,8 +1,14 @@
 package com.cos.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,11 +18,28 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
+// html 파일이 아니라 data 를 리턴해주는 controller = RestController 
 @RestController
 public class DummyControllerTest {
 	
 	@Autowired // 의존성 주입 (DI)
 	private UserRepository userRepositroy;
+	
+	// http://localhost:8000/blog/dummy/user
+	@GetMapping("/dummy/users")
+	public List<User> list(){
+		
+		return userRepositroy.findAll();
+	}
+	
+	// 한 페이지당 2건의 데이터를 리턴받아 볼 예정 
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=2, sort="id",direction = Sort.Direction.DESC) Pageable pageable){
+		
+		List<User> users = userRepositroy.findAll(pageable).getContent();
+		
+		return users;
+	}
 	
 	// {id} 주소로 파라미터를 전달 받을 수 있음.
 	// http://localhost:8000/blog/dummy/user/3
@@ -40,6 +63,12 @@ public class DummyControllerTest {
 			}
 		});
 		
+		// 요청 : 웹브라우저 
+		// user 객체 = 자바 오브젝트 
+		// 변환 (웹브라우저가 이해할 수 있는 데이터) -> JSON (Gson 라이브러리) 
+		// 스프링부트 = MessageConverter 라는 애가 응답시에 자동 작동 
+		// 만약에 자바 오즈젝트를 리턴하게 되면 MessageConverter가 Jackson 이라는 라이브러리를 호출해서
+		// user 오브제특를 json으로 변환해서 브라우저에게 던져줍니다. 
 		return user;
 	}
 	
