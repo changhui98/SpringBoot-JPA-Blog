338b71fffa56df292bc2369acad81fef2b59d065
unknown <net_0102@naver.com>
2024-08-07 21:49:27 +0900

    회원가입 기능 구현 완료 (DB COMMIT)

---
+++ src/main/java/com/cos/blog/dto/ResponseDto.java
@@ -0,0 +1,19 @@
+package com.cos.blog.dto;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class ResponseDto<T> {
+	
+	HttpStatus status;
+	T data;
+
+}
