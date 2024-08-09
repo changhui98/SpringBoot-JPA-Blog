e5c7a6b961e48caf2c3595b77667684a41fbc29e
unknown <net_0102@naver.com>
2024-08-09 17:31:26 +0900

    JPA 로그인 기능 코드 생성

--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.cos.blog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.cos.blog.model.User;
 
@@ -8,6 +9,13 @@ import com.cos.blog.model.User;
 // 자동으로 bean 등록이 된다. 
 // @Repository 생략 가능하다. 
 public interface UserRepository extends JpaRepository<User, Integer>{
-
 	
+	// JPA Naming 쿼리
+	// SELECT * FROm user WHERE username = ? AND password = ?;
+	User findByUsernameAndPassword(String username, String password);
+	
+	/*
+	 * @Query(value="SELECT * FROm user WHERE username = ?1 AND password = ?2",
+	 * nativeQuery = true) User login(String username, String password);
+	 */
 }
--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -2,11 +2,12 @@ package com.cos.blog.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
-import jakarta.transaction.Transactional;
+
 
 // 스프링이 컴포넌트 스캔을 통해서 Bean 에 등록을 해줌. IoC 를 해준다.
 @Service
@@ -20,5 +21,12 @@ public class UserService {
 		
 		userRepository.save(user);
 	}
+	
+	@Transactional(readOnly = true) // select 할 때 트랜잭션 시작. 서비스 종료시에 트랜잭션 종료 (정합성 유지) 
+	public User 로그인(User user) {
+		
+		return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
+	}
+	
 		
 }
