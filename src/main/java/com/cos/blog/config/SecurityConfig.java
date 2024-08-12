532e2607d9c65049512222753524cfafcff8fe54
unknown <net_0102@naver.com>
2024-08-12 22:21:45 +0900

    스프링 시큐리티 버전 업그레이드로 변경 된 부분 수정 이전

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.cos.blog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,27 +12,40 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import jakarta.security.auth.message.config.AuthConfig;
 
-// 빈 등록 : 스프링 컨테이너에서 객체를 관리할 수 있게 하는 것 
+// 빈 등록 : 스프링 컨테이너에서 객체를 관리할 수 있게 하는 것
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소로 접근을 하면 권한 및 인증을 미리 체크하겠다는 뜻
+
+/**
+ * 스프링 시큐리티 버전 업그레이드 되면서, 위 두가지 어노테이션 제거.
+ */
+
 @Configuration // 빈 등록 IoC 관리
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소로 접근을 하면 권한 및 인증을 미리 체크하겠다는 뜻
-public class SecurityConfig  {
-	
+public class SecurityConfig {
+
+	// ** spring security update : principalDetailService 제거
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration anthenAuthenticationConfiguration)
+			throws Exception {
+		return anthenAuthenticationConfiguration.getAuthenticationManager();
+	}
+
 	@Bean // IoC가 됨.
 	public BCryptPasswordEncoder encodePWD() {
-		
+
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Bean
 	SecurityFilterChain configure(HttpSecurity http) throws Exception {
-		http
-		.csrf().disable() // csrf 토큰 비활성화 (테스트시 걸어두는 게 좋음)
-		.authorizeHttpRequests(auth -> auth.requestMatchers("/","/auth/**", "/WEB-INF/**", "/js/**", "/css/**", "/image/**").permitAll()
-				.anyRequest().authenticated());
-		
+		http.csrf().disable() // csrf 토큰 비활성화 (테스트시 걸어두는 게 좋음)
+				.authorizeHttpRequests(
+						auth -> auth.requestMatchers("/", "/auth/**", "/WEB-INF/**", "/js/**", "/css/**", "/image/**")
+								.permitAll().anyRequest().authenticated());
+
 		http.formLogin(f -> f.loginPage("/auth/loginForm").permitAll());
-		
+
 		return http.build();
 	}
 
--- src/main/webapp/WEB-INF/views/user/loginForm.jsp
+++ src/main/webapp/WEB-INF/views/user/loginForm.jsp
@@ -3,7 +3,7 @@
 <%@ include file="../layout/header.jsp"%>
 
 <div class="container">
-	<form action="#" method="post">
+	<form action="/auth/loginProc" method="post">
 		<div class="form-group">
 			<label for="username">User Name :</label> 
 			<input type="text" name="username" class="form-control" placeholder="Enter User Name" id="username">
@@ -14,17 +14,8 @@
 			<input type="password"  name="password" class="form-control" placeholder="Enter password" id="password">
 		</div>
 		
-		<div class="form-group form-check">
-			<label class="form-check-label"> 
-			<input class="form-check-input"  name="remember" type="checkbox"> Remember me
-			</label>
-		</div>
-		
 		<button id="btn-login" class="btn btn-primary">Login</button>
 	</form>
-	
-	
-
 </div>
 
 <script src="/js/user.js"></script>
