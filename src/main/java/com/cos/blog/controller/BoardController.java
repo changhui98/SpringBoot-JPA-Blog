4ce1ac1faf24a5d9f65a8d3126075ae990548907
unknown <net_0102@naver.com>
2024-08-14 22:13:23 +0900

    메인페이지 글목록 보기 기능 구현

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,13 +1,23 @@
 package com.cos.blog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cos.blog.service.BoardService;
 
 @Controller
 public class BoardController {
+	
+	@Autowired
+	private BoardService boardService;
 
+	// 컨트롤러에서 세션을 어떻게 찾는지 ?
+	// @AuthenticationPrincipal PrincipalDetail principal
 	@GetMapping({"", "/"})
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("boards", boardService.글목록());
 		
 		// WEB-INF/views/index.jsp
 		return "index";
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -3,14 +3,17 @@
 <%@ include file="layout/header.jsp"%>
 
 <div class="container">
-	<div class="card m-2">
 
-		<div class="card-body">
-			<h4 class="card-title">Title</h4>
-			<a href="#" class="btn btn-primary">See more..</a>
+	<c:forEach var="board" items="${boards }">
+		<div class="card m-2">
+
+			<div class="card-body">
+				<h4 class="card-title">${board.title }</h4>
+				<a href="#" class="btn btn-primary">See more..</a>
+			</div>
 		</div>
-		
-	</div>
+	</c:forEach>
+
 </div>
 
 <%@ include file="layout/footer.jsp"%>
